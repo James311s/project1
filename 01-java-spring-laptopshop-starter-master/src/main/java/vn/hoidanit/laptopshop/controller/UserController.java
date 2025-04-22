ea75b0296323e695503aa5400724fd099659a42d
James Nguyen <jamesnguyen311s@gmail.com>
2025-04-22 21:41:19 +0700

    update22402

--- 01-java-spring-laptopshop-starter-master/src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ 01-java-spring-laptopshop-starter-master/src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -9,17 +9,33 @@ import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.service.UserService;
 
-@RestController
+@Controller
 public class UserController {
+
     private UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("")
+    @RequestMapping("/")
     public String getHomePage() {
-        return this.userService.handleHello();
+        String test = this.userService.handleHello();
+        return "hello";
     }
-
 }
+
+// @RestController
+// public class UserController {
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+
+// }
