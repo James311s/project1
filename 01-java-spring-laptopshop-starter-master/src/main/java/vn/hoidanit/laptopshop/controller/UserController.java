4c4492db6554385409ba75a2f2a40211dd60975d
James Nguyen <jamesnguyen311s@gmail.com>
2025-05-03 09:41:44 +0700

    update5325

--- 01-java-spring-laptopshop-starter-master/src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ 01-java-spring-laptopshop-starter-master/src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -3,12 +3,14 @@ package vn.hoidanit.laptopshop.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.service.UserService;
+import vn.hoidanit.laptopshop.domain.User;
 
 @Controller
 public class UserController {
@@ -26,6 +28,18 @@ public class UserController {
         model.addAttribute("name", "James");
         return "hello";
     }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User Jamesnguyen) {
+        System.out.println("run đi nha" + Jamesnguyen);
+        return "hello";
+    }
 }
 
 // @RestController
@@ -40,5 +54,4 @@ public class UserController {
 // public String getHomePage() {
 // return this.userService.handleHello();
 // }
-
 // }
